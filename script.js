83329ca9da5d6031368c453aa879f5c021ca5920
Imma-codes <immaculatambama@gmail.com>
2026-06-27 16:11:25 +0100

    improved visual design

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links a");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
